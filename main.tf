7a34dd6e490cd71504d02b437afdcdfda47fd388
EC2 Default User <ec2-user@ip-172-31-89-18.ec2.internal>
2026-01-31 15:45:05 +0000

    Updated Terraform for EC2 and ECR repositories

--- main.tf
+++ main.tf
@@ -11,38 +11,65 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "clo835_sg" {
-  name        = "clo835-ec2-sg"
-  description = "Security group for CLO835 EC2 instance"
+# -----------------------------
+# ECR Repositories
+# -----------------------------
+resource "aws_ecr_repository" "webapp" {
+  name         = "clo835-webapp"
+  force_delete = true
+}
 
+resource "aws_ecr_repository" "mysql" {
+  name         = "clo835-mysql"
+  force_delete = true
+}
+
+# -----------------------------
+# Default VPC + Subnet
+# -----------------------------
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# -----------------------------
+# Amazon Linux 2 AMI
+# -----------------------------
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# -----------------------------
+# Security Group
+# -----------------------------
+resource "aws_security_group" "web_sg" {
+  name   = "clo835-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  # App ports
   ingress {
-    description = "SSH"
+    from_port   = 8081
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SSH
+  ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "App Port 8081"
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "App Port 8082"
-    from_port   = 8082
-    to_port     = 8082
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "App Port 8083"
-    from_port   = 8083
-    to_port     = 8083
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -55,11 +82,17 @@ resource "aws_security_group" "clo835_sg" {
   }
 }
 
-resource "aws_instance" "clo835_ec2" {
-  ami                    = "ami-0c02fb55956c7d316"
-  instance_type          = "t2.micro"
-  key_name               = "vockey"
-  vpc_security_group_ids = [aws_security_group.clo835_sg.id]
+# -----------------------------
+# EC2 Instance
+# -----------------------------
+resource "aws_instance" "ec2" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  key_name                    = "vockey"
+  associate_public_ip_address = true
+  iam_instance_profile        = "LabInstanceProfile"
 
   tags = {
     Name = "clo835-ec2"
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
+# -----------------------------
+# Outputs
+# -----------------------------
 output "ec2_public_ip" {
-  value = aws_instance.clo835_ec2.public_ip
+  value = aws_instance.ec2.public_ip
 }
 
-output "ec2_public_dns" {
-  value = aws_instance.clo835_ec2.public_dns
+output "webapp_ecr_url" {
+  value = aws_ecr_repository.webapp.repository_url
 }
 
-
+output "mysql_ecr_url" {
+  value = aws_ecr_repository.mysql.repository_url
+}
